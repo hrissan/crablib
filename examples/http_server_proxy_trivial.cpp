45a90e9e7fde8e7cb139841abd698855dbb3c748
Grigory Buteyko <hrissan@mail.ru>
2020-06-13 01:24:40 +0300

    another req example

--- examples/http_server_proxy_trivial.cpp
+++ examples/http_server_proxy_trivial.cpp
@@ -42,6 +42,35 @@ private:
 	std::list<http::ClientRequestSimple> waiting_requests;
 };
 
+class ServerProxyTrivial2 {
+public:
+	explicit ServerProxyTrivial2(uint16_t port) : server(port) {
+		server.r_handler = [&](http::Client *who, http::Request &&request) {
+			auto req     = std::make_shared<http::ClientRequestSimple>();
+			auto counter = ++next_counter;
+			req->set_handlers(
+			    [who, counter](http::Response &&resp) {
+				    std::cout << "Success " << counter << std::endl;
+				    who->write(std::move(resp));
+			    },
+			    [who, counter](std::string &&err) {
+				    std::cout << "Error " << counter << std::endl;
+				    who->write(http::Response::simple_text(503, std::move(err)));
+			    });
+			std::cout << "Sending request " << counter << std::endl;
+			request.header.host = "www.alawar.com";
+			req->send(std::move(request), 443, "https");
+			who->start_long_poll([req, counter]() {  // Owns req
+				std::cout << "Disconnect " << counter << std::endl;
+			});
+		};
+	}
+
+private:
+	http::Server server;
+	int next_counter = 0;
+};
+
 int main(int argc, char *argv[]) {
 	std::cout << "crablib version " << crab::version_string() << std::endl;
 
@@ -54,6 +83,7 @@ int main(int argc, char *argv[]) {
 	});
 
 	ServerProxyTrivial app(7000);
+	ServerProxyTrivial2 app2(7001);
 
 	runloop.run();
 	return 0;
